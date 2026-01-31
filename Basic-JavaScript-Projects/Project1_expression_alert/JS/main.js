c019721d4bd2d6f082ee5fe60902df059c4776d7
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-01-31 14:52:16 -0700

    Basic JavaScript Assignment
    This asignment submission is for Step 65 in the JavaScript Submission Assignemnt

---
+++ Basic-JavaScript-Projects/Project1_expression_alert/JS/main.js
@@ -0,0 +1,15 @@
+// Sentence 1
+var sent1 = 'This is the begninning of the string'
+
+// Sentence 2
+var sent2 = 'and this is the end of the string.'
+
+// Output: This is the begninning of the string and this is the end of the string.
+var fullSentence = sent1 + ' ' + sent2;
+console.log(fullSentence);
+
+// Window alert
+alert(fullSentence);
+
+// Document write
+document.write(fullSentence);
