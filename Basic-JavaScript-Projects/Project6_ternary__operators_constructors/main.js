69169e0be8b6a85122a3d0d710aeaada18d0bcf5
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-02-10 23:02:37 -0700

    Scope Functions
    Here is my submission for step 153.

--- Basic-JavaScript-Projects/Project6_ternary__operators_constructors/main.js
+++ Basic-JavaScript-Projects/Project6_ternary__operators_constructors/main.js
@@ -1,41 +1,10 @@
 
-// Ternary Operator Condition
-function runCheck() {
-    let age = 20;
-    const result = age >= 18 ? "Adult." : "Minor.";
-    document.getElementById('output').innerHTML = result;
+var x = 10;
+function Add_numbers_1() {
+    document.write(20 + x + "<br>");
 }
-
-// Constructor function 
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-}
-
-const user1 = new Person('Saaffe', 29);
-console.log(user1.name); // Output: Saaffe
-
-// connecting a new constructor function to html
-
-function Cat(name) {
-    this.name = name;
-}
-
-function runCode() {
-    const myCat = new Cat('Whiskers');
-    document.getElementById('result').innerText = myCat.name;
-}
-
-// Nested function 
-function outerFunction() {
-    let count = 0;
-
-    function innerFunction() {
-        count++;
-        return count;
-    }
-
-    return innerFunction;
-}
-
-console.log(outerFunction(10))
+function Add_numbers_2() {
+    document.write(x + 100);
+};
+Add_numbers_1();
+Add_numbers_2();
---
+++ Basic-JavaScript-Projects/Project7_scope_time_function/main.js
@@ -0,0 +1,48 @@
+
+// Assigning a global and local variable 
+let globalVariable = "I am global"; 
+
+function example() {
+  let localVariable = "I am local"; // Local var
+  console.log(globalVariable, localVariable);
+}
+example();
+
+// write a function that uses an if else statement 
+// feel free to change the hour of your liking to see the different outputs
+let hour = 10;
+
+if (hour >= 6 && hour < 12) 
+    console.log('Good morning!');
+else if (hour >= 12 && hour < 18)
+    console.log('Good Afternoon');
+else 
+    console.log('Good Evening');
+
+// write a function that throws an error
+
+function add(a, b) {
+    console.log("a =", a);
+    console.log("b =", b);
+    return a + c; // intentional error: c is not defined
+  }
+  
+  add(2, 3);
+// This error shoule be fixed since c is not defined 
+
+// Time function from step 151 
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
