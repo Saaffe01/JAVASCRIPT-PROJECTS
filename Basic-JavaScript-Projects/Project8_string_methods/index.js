8eebf62aaa5af089b2fd421974cce56e13f42054
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-02-11 20:50:47 -0700

    String Method Submission
    Here is my submission for step 167. Here we use a few string conversion methods.

--- Basic-JavaScript-Projects/Project8_string_methods/index.js
+++ Basic-JavaScript-Projects/Project8_string_methods/index.js
@@ -1,5 +1,32 @@
-function string_Method() {
-  let x = 182;
-  document.getElementById("Numbers_to_string").innerHTML = x.toString();
+
+// writing functions to for each method: concat(), slice(), toString(), toPrecise()
+
+// concat() method 
+function concat() {
+    return [1, 2].concat([3, 4]);
+}
+
+// slice() method
+function slice() {
+    return [10, 20, 30].slice(0, 2);
+}
+
+// toString() method
+function stringConversion() {
+    return (1234).toString();
+}
+
+// toPrecision() method
+function preciseConversion() {
+    return (3.14159).toPrecision(3);
+}
+
+
+// These functions run in the browser .
+function results() {
+    document.getElementById('concatResult').textContent = "concat(): " + concat();
+    document.getElementById('sliceResult').textContent = "slice(): " + slice();
+    document.getElementById('stringConversionResult').textContent = "toString(): " + stringConversion();
+    document.getElementById('preciseConversionResult').textContent = "toPrecision(): " + preciseConversion();
 }
 
