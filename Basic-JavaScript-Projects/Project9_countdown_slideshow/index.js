4590d2ba6018826c0e7d25a610bd7246431d0b7f
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-02-12 21:07:20 -0700

    Countdown Slideshow Project
    Here is my submission for the slideshow project we used, with html, css, and javascript

--- Basic-JavaScript-Projects/Project9_countdown_slideshow/index.js
+++ Basic-JavaScript-Projects/Project9_countdown_slideshow/index.js
@@ -1,35 +1,35 @@
 const slides = ["images/corvette.jpg", "images/lamborghini.jpg", "images/sf90.jpg"];
-        let currentIndex = 0;
-        let timeLeft = 5;
+    let currentIndex = 0;
+    let timeLeft = 5;
 
-        // countdown function
-        function countdown() {
-            timeLeft--;
-            
-            if (timeLeft <= 0) {
-                changeSlide(1); 
-            }
-            
-            document.getElementById("timer").innerText = timeLeft;
+    // countdown function
+    function countdown() {
+        timeLeft--;
+        
+        if (timeLeft <= 0) {
+            changeSlide(1); 
         }
+        
+        document.getElementById("timer").innerText = timeLeft;
+    }
+
+    // function to change the slide
+    function changeSlide(direction) {
+        currentIndex += direction;
 
 
-        function changeSlide(direction) {
-            currentIndex += direction;
+        if (currentIndex >= slides.length) currentIndex = 0;
+        if (currentIndex < 0) currentIndex = slides.length - 1;
 
+        // updating the html
+        document.getElementById("slide-display").src = slides[currentIndex];
 
-            if (currentIndex >= slides.length) currentIndex = 0;
-            if (currentIndex < 0) currentIndex = slides.length - 1;
+        timeLeft = 5; 
+        document.getElementById("timer").innerText = timeLeft;
+    }
 
-            // updating the html
-            document.getElementById("slide-display").src = slides[currentIndex];
+        function manualChange(direction) {
+        changeSlide(direction);
+    }
 
-            timeLeft = 5; 
-            document.getElementById("timer").innerText = timeLeft;
-        }
-
-            function manualChange(direction) {
-            changeSlide(direction);
-        }
-
-        setInterval(countdown, 1000);
+    setInterval(countdown, 1000);
